82001dda320281aa7b193fc7df73c164c54c050a
fxecc <135511611+fxecc@users.noreply.github.com>
2023-12-31 16:08:28 +0800

    Update check.php

--- check.php
+++ check.php
@@ -1,55 +1,69 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $url = $_POST['url'];
+    $urlList = explode("\n", $_POST['urls']);
+    $urlList = array_map('trim', $urlList);
+    $urlList = array_filter($urlList);
 
-    // 添加默认的http协议前缀
+    $responses = [];
+
+    $concurrentLimit = 5; // 控制并发请求数量
+
+    $chunks = array_chunk($urlList, $concurrentLimit);
+
+    foreach ($chunks as $chunk) {
+        $promises = [];
+        $mh = curl_multi_init();
+
+        foreach ($chunk as $url) {
+            $url = addHttpProtocol($url);
+
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+
+            curl_multi_add_handle($mh, $ch);
+
+            $promises[$url] = $ch;
+        }
+
+        do {
+            curl_multi_exec($mh, $running);
+        } while ($running > 0);
+
+        foreach ($promises as $url => $ch) {
+            $result = curl_multi_getcontent($ch);
+            $info = curl_getinfo($ch);
+
+            $title = '';
+            if ($info['http_code'] === 200) {
+                preg_match('/<title>(.*?)<\/title>/', $result, $matches);
+                if (isset($matches[1])) {
+                    $title = $matches[1];
+                }
+            }
+
+            $responses[] = [
+                'url' => $url,
+                'title' => $title,
+                'status' => $info['http_code'] === 200 ? '正常' : '失败',
+            ];
+
+            curl_multi_remove_handle($mh, $ch);
+            curl_close($ch);
+        }
+
+        curl_multi_close($mh);
+    }
+
+    echo json_encode($responses);
+    exit;
+}
+
+function addHttpProtocol($url) {
     if (!preg_match("~^(?:f|ht)tps?://~i", $url)) {
         $url = 'http://' . $url;
     }
-
-    // 使用cURL请求URL并获取网站标题
-    $handle = curl_init();
-    curl_setopt($handle, CURLOPT_URL, $url);
-    curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($handle, CURLOPT_TIMEOUT, 10);
-    $result = curl_exec($handle);
-    $info = curl_getinfo($handle);
-    $title = '';
-    if ($info['http_code'] === 200) {
-        preg_match('/<title>(.*?)<\/title>/', $result, $matches);
-        if (isset($matches[1])) {
-            $title = $matches[1];
-        }
-    }
-    curl_close($handle);
-
-    // 如果http协议检测失败，则尝试https协议
-    if ($info['http_code'] !== 200) {
-        $httpsUrl = str_replace('http://', 'https://', $url);
-
-        $handle = curl_init();
-        curl_setopt($handle, CURLOPT_URL, $httpsUrl);
-        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($handle, CURLOPT_TIMEOUT, 10);
-        $result = curl_exec($handle);
-        $info = curl_getinfo($handle);
-        if ($info['http_code'] === 200) {
-            preg_match('/<title>(.*?)<\/title>/', $result, $matches);
-            if (isset($matches[1])) {
-                $title = $matches[1];
-            }
-        }
-        curl_close($handle);
-    }
-
-    // 返回检测结果
-    $response = [
-        'success' => true,
-        'url' => $url,
-        'title' => $title,
-        'status' => $info['http_code'] === 200 ? '正常' : '失败',
-    ];
-    echo json_encode($response);
-    exit;
+    return $url;
 }
 ?>
